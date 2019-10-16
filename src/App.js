9aa50a1a78393de5c934891e2ff6f9fef61fb29f
Mike Torres <apolloproductionsdj@gmail.com>
2019-10-16 05:00:42 +0000

    added the List component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import Nav from './components/Nav';
+import List from './components/List';
 
 class App extends Component {
 render() {
   return (
     <div className="App">
       <div>
-        Nav Component
+        <Nav />
+        <List />
       </div>
     </div>
   );
